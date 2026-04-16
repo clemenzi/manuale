09137ea122ee8e769e0536a25644eb11f247c38c
clemenzi <nc@vcz.me>
2026-04-16 17:08:35 +0200

    feat: reworded all the docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import { cloudflare } from '@cloudflare/vite-plugin'
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from 'fumadocs-mdx/vite';
-import { nitro } from 'nitro/vite';
 
 export default defineConfig({
   server: {
@@ -37,8 +37,7 @@ export default defineConfig({
       ],
     }),
     react(),
-    // please see https://tanstack.com/start/latest/docs/framework/react/guide/hosting#nitro for guides on hosting
-    nitro(),
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
   ],
   resolve: {
     tsconfigPaths: true,
